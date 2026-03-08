49c7b481e33ec05f4e1b97274785ce2760945103
Julia <Julia.turko7@gmail.com>
2026-03-09 01:16:28 +0200

    cors resolve

--- snippett/src/main.ts
+++ snippett/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin:"http://localhost:3001",
+    origin: ["http://localhost:3001", "https://snippets-test.vercel.app/"],
     methods:'GET, HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credential: true
   })
